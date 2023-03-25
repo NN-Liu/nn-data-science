c842701f42434338befa723fd236930e6a38a9fc
Niannian Liu <100664722+NN-Liu@users.noreply.github.com>
2023-03-25 14:43:31 -0500

    re-format

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ def contact():
 @app.route('/car_regression', methods=["GET", "POST"])
 def car_regression():
     try:
-            model_car = pickle.load(open("model_car.pkl", "rb"))
-            int_features = [float(x) for x in request.form.values()]
-            final_features = [np.array(int_features)]
-            prediction = model_car.predict(final_features)
+        model_car = pickle.load(open("model_car.pkl", "rb"))
+        int_features = [float(x) for x in request.form.values()]
+        final_features = [np.array(int_features)]
+        prediction = model_car.predict(final_features)
         if request.method == "POST":
             output = round(prediction[0], 2)
         else:
@@ -38,10 +38,10 @@ def car_regression():
 @app.route('/iris_classification', methods=["GET", "POST"])
 def iris_classification():
     try:
-            variety_mappings = {0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'}
-            model_iris = pickle.load(open("model_iris.pkl", "rb"))
-            float_features = [float(x) for x in request.form.values()]
-            final_features = [np.array(float_features)]
+        variety_mappings = {0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'}
+        model_iris = pickle.load(open("model_iris.pkl", "rb"))
+        float_features = [float(x) for x in request.form.values()]
+        final_features = [np.array(float_features)]
         if request.method == "POST":
             prediction = variety_mappings[model_iris.predict(final_features)[0]]  # Retrieve from dictionary
 
