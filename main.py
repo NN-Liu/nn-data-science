439e385cbc6c3eda5aa0341e4e1dad0d1f684efd
Niannian Liu <niannianliu@imac.lan>
2023-03-26 20:08:41 -0500

    update files

--- main.py
+++ main.py
@@ -11,8 +11,13 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/car_regression', methods=["GET", "POST"])
+@app.route('/car_regression', methods=["GET"])
 def car_regression():
+    return render_template("car_regression.html")
+
+
+@app.route('/car_regression_result', methods=["POST"])
+def car_regression_result():
     try:
         dataset = pd.read_csv('csv/used_honda_price.csv')
         X = dataset.iloc[:, :-1].values
@@ -24,29 +29,33 @@ def car_regression():
         regressor.fit(X, y)
 
         # Saving model to current directory
-        pickle.dump(regressor, open("model_car.pkl", "wb"))
+        pickle_out = open("model_car.pickle", "wb")
+        pickle.dump(regressor, pickle_out)
+        pickle_out.close()
 
-        if request.method == "POST":
-            model_car = pickle.load(open("model_car.pkl", "rb"))
-            int_features = [float(x) for x in request.form.values()]
-            final_features = [np.array(int_features)]
-            prediction = model_car.predict(final_features)
-            output = round(prediction[0], 2)
-        else:
-            output = ""
-        return render_template("car_regression.html", output=output)
+        model_car = pickle.load(open("model_car.pickle", "rb"))
+        int_features = [float(x) for x in request.form.values()]
+        final_features = [np.array(int_features)]
+        prediction = model_car.predict(final_features)
+        output = round(prediction[0], 2)
+        return render_template("car_regression_result.html", output=output)
     except:
         return "An error has incurred."
 
 
-@app.route('/iris_classification', methods=["GET", "POST"])
+@app.route('/iris_classification', methods=["GET"])
 def iris_classification():
+    return render_template("iris_classification.html")
+
+
+@app.route('/iris_classification_result', methods=["POST"])
+def iris_classification_result():
     try:
+
         # Importing the dataset
         dataset = pd.read_csv('csv/iris.csv')
 
         # Mapping varieties to dictionary
-        variety_mappings = {0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'}
         dataset = dataset.replace(['Setosa', 'Versicolor', 'Virginica'], [0, 1, 2])
         X = dataset.iloc[:, :-1].values
         y = dataset.iloc[:, -1].values
@@ -57,16 +66,17 @@ def iris_classification():
         classifier.fit(X, y)
 
         # Saving model to current directory
-        pickle.dump(classifier, open("model_iris.pkl", "wb"))
+        pickle_out = open("model_iris.pickle", "wb")
+        pickle.dump(classifier, pickle_out)
+        pickle_out.close()
 
-        if request.method == "POST":
-            model_iris = pickle.load(open("model_iris.pkl", "rb"))
-            float_features = [float(x) for x in request.form.values()]
-            final_features = [np.array(float_features)]
-            prediction = variety_mappings[model_iris.predict(final_features)[0]]  # Retrieve from dictionary
-        else:
-            prediction = ""
-        return render_template("iris_classification.html", prediction=prediction)
+        variety_mappings = {0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'}
+        model_iris = pickle.load(open("model_iris.pickle", "rb"))
+        float_features = [float(x) for x in request.form.values()]
+        final_features = [np.array(float_features)]
+        prediction = variety_mappings[model_iris.predict(final_features)[0]]  # Retrieve from dictionary
+
+        return render_template("iris_classification_result.html", prediction=prediction)
     except:
         return "An error has incurred."
 
@@ -92,4 +102,4 @@ def nlp():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
